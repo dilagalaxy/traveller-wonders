f480b9aa44a7ef079eed7e1b206945a4bbc24684
Siti Nuradila <startbeautiful@gmail.com>
2017-02-24 19:02:31 +0800

    changes for my <<HEAD error

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -11,16 +10,10 @@ require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
-<<<<<<< HEAD
-module Nameofapp
-=======
 module TravellerWonders
->>>>>>> b1f71d6cd54511936b14c4f4e2774331d2206377
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
