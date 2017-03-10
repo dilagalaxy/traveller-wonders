0e8d019e1bf3cfb4468d76e82a4e57dd613b4ea9
Siti Nuradila <startbeautiful@gmail.com>
2017-03-10 23:30:07 +0800

    changes for 4.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307130354) do
+ActiveRecord::Schema.define(version: 20170310114232) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20170307130354) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "color"
+    t.string   "price"
   end
 
 end
