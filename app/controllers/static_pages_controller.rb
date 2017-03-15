26c45c76d1e509f7b924de536bd17acb898fa2e5
Siti Nuradila <startbeautiful@gmail.com>
2017-03-15 22:38:09 +0800

    4.5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
   end
 
   def landing_page
-    @products = Product.limit(3)
-    @featured_product = Product.first
+    @pfeatured_products = Product.limit(3)
+    @featured_product = Product.second
   end
 end
