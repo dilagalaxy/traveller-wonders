e332f1ca8440deedeecd3ed9f88806048fdc4237
Siti Nuradila <startbeautiful@gmail.com>
2017-03-15 21:58:22 +0800

    4.5

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,25 +1,9 @@
 class StaticPagesController < ApplicationController
-
- def index
- end
-
-  def about
-  end
-
-  def contact
+  def index
   end
 
   def landing_page
+    @products = Product.limit(3)
     @featured_product = Product.first
   end
-
-  def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
-
-    UserMailer.contact_form(@email, @name, @message).deliver_now
-
-  end
-
 end
