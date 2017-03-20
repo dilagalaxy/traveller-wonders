759a35a838ac105e0a1db76d7e5f1f08a578e4ab
Siti Nuradila <startbeautiful@gmail.com>
2017-03-20 21:26:06 +0800

    4.6

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,7 @@ ActiveRecord::Schema.define(version: 20170318031144) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
     t.string   "price"
   end
 
