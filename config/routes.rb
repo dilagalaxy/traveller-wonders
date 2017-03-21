d77516247b53ce0d15836d8f75e228396acf91a9
Siti Nuradila <startbeautiful@gmail.com>
2017-03-21 17:24:42 +0800

    5.1

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,15 @@ Rails.application.routes.draw do
     resources :comments
   end
 resources :users
-  root 'static_pages#landing_page'
+  get 'static_pages/landing_page'
 
   get 'static_pages/landing_page'
 
-  get 'static_pages/about'
+   get 'static_pages/about'
 
   get 'static_pages/contact'
-
-  get 'static_pages/index'
+   
+  root 'static_pages#index'
 
 
  resources :orders, only:
