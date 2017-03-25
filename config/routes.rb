5b029bd52fa8cdbe3764f0ae66d0c01d9727c2cb
Siti Nuradila <startbeautiful@gmail.com>
2017-03-25 16:18:17 +0800

    5.3 changes

--- config/routes.rb
+++ config/routes.rb
@@ -11,14 +11,15 @@ resources :users
 
    get 'static_pages/about'
 
-  root 'static_pages#contact'
+   get'static_pages/contact'
    
   root 'static_pages#index'
-
-  post 'static_pages/thank_you'
   
  resources :orders, only:
 [:index, :show, :create, :destroy]
+
+
+  post 'static_pages/thank_you'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
