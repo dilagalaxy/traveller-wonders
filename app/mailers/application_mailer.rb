032b2e3e0601a8f801ba4bdde58085a74717f37b
Siti Nuradila <startbeautiful@gmail.com>
2017-03-25 08:37:02 +0800

    changes for validation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'startbeautiful@heroku.com'
+  default from: 'startbeautiful@gmail.com'
   layout 'mailer'
 end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
- 	default from: 'startbeautiful@heroku.com'
+ 	default from: 'startbeautiful@gmail.com'
  
  	def contact_form(email, name, message)
  	@message = message
