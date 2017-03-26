84345887038b3fa73f60fa0593788cfaf0e768ab
Siti Nuradila <startbeautiful@gmail.com>
2017-03-26 19:22:49 +0800

    5.3 CHANGES

--- config/environments/environment.rb
+++ config/environments/environment.rb
@@ -4,8 +4,8 @@ ActionMailer::Base.smtp_settings = {
   	:address => 'smtp.sendgrid.net',
   	:port => '587',
   	:authentication => :plain,
-  	:user_name => ENV['SENDGRID_USERNAME'],
+  	:user_name => ENV['startbeautiful@heroku.com'],
     :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
+    :domain => 'travellerwonders.herokuapp.com',
     :enable_starttls_auto => true
 }
