f0667c15fb88a81907d9bc4a7581333202d63c22
Siti Nuradila <startbeautiful@gmail.com>
2017-03-26 11:05:12 +0800

    5.3 changes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,13 +8,13 @@ class StaticPagesController < ApplicationController
    end
 
       def thank_you
-    @name = params[:name]
-    @email = params[:email]
-    @message = params[:message]
+         @name = params[:name]
+         @email = params[:email]
+         @message = params[:message]
    
-   ActionMailer::Base.mail(:from => @email,
+    ActionMailer::Base.mail(:from => @email,
         :to => 'startbeautiful@gmail.com',
         :subject => "A new contact form message from #{@name}",
         :body => @message).deliver_now
-  end
-end
+    end
+ end
