3601978c2854473802a2d1c823e1d3b20a796fcf
Siti Nuradila <startbeautiful@gmail.com>
2017-03-26 20:38:08 +0800

    5.3 changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # config.active_job.queue_name_prefix = "traveller-wonders_#{Rails.env}"
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.default_url_options = { :host => "http://www.travellerwonders.herokuapp.com" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
@@ -71,7 +71,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.action_mailer.default_url_options = { :host => "http://www.travellerwonders.herokuapp.com" }
+  
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
