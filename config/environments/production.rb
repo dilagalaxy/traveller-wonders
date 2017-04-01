620553fc1a61b255fb29f8d01a464aa118c97004
Siti Nuradila <startbeautiful@gmail.com>
2017-04-01 23:23:50 +0800

    5.4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,7 +57,7 @@ Rails.application.configure do
 
   # config.active_job.queue_name_prefix = "traveller-wonders_#{Rails.env}"
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { :host => "http://www.travellerwonders.herokuapp.com" }
+  config.action_mailer.default_url_options = { :host => "http://travellerwonders.herokuapp.com" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
