2044e943abfca15e7b3556540a5979467fa57a61
Siti Nuradila <startbeautiful@gmail.com>
2017-04-08 10:22:01 +0800

    5.8

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,11 @@
-
- $(document).on("turbolinks:load", function () {
-   $(".rating").raty( { path: '/assets/images/images', scoreName: 'comment[rating]' });
-   $(".rated").raty({ path: "/assets/images/images",
-     readOnly: true,
-     score: function() {
-       return $(this).attr("data-score");
-     }
-   });
- 
+$(document).on('turbolinks:load', function(){
+$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+$('.rated').raty({ path: '/assets',
+readOnly: true,
+score: function() {
+return $(this).attr('data-score');
+}
+});
    $(".enable-review").click( function() {
      $(".review-container").show();
    });
