7d9f9285f7f953b47a91f11e36e017d281a41cd6
Siti Nuradila <startbeautiful@gmail.com>
2017-04-12 12:34:22 +0800

    5.10

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,12 +4,14 @@ class Ability
   def initialize(user)
     user ||= User.new #guest user (not logged in)
     
-    can :manage, User, id: user.id  
-
-    if user.admin?
-      can :destroy, Comment
+    can :create, Comment
+      can :read, Comment
+      can :read, Product
+ 
+      if user.admin?
+        can :destroy, Comment
         can :manage, Product
-      can :destroy, User      
+        can :destroy, User      
       end
     
   end
