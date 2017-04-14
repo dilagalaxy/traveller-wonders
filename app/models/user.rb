e17db3184658f29a5f9cb636e73adb1007cb2bd1
Siti Nuradila <startbeautiful@gmail.com>
2017-04-14 22:15:21 +0800

    5.11

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ActiveRecord::Base
     has_many :orders
     has_many :comments
 
-    after_create :send_admin_mail
+    after_create : welcome_mail
   def send_admin_mail
-    UserMailer.send_new_user_message(self).deliver
+    UserMailer.welcome_message(self).deliver
   end
 
  end
