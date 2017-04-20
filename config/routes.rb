c0d96b36f0332a9a293f2cb24dc187eae026df53
Siti Nuradila <startbeautiful@gmail.com>
2017-04-21 00:12:42 +0800

    6.3

--- config/routes.rb
+++ config/routes.rb
@@ -2,22 +2,16 @@ Rails.application.routes.draw do
 devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, :controllers => { :registrations => "user_registrations" }
 
   resources :products do 
-  resources :comments 
-  resources :users
-  resources :products
-  resources :orders, only: [:index, :show, :create, :destroy]
-   end
+   resources :comments
+end 
 
-  get 'static_pages/landing_page'
+  resources :users
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   get 'static_pages/landing_page'
   get 'static_pages/about'
   get'static_pages/contact'  
   post 'static_pages/thank_you'
+
   root 'static_pages#index'
-  post '/reset_password', to: 'reset_password#create', as: 'send_reset_password'
- resources :password_resets,     only: [:new, :create, :edit, :update]
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
 end
