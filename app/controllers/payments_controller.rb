c9679e934ebe0af3742e2827371a946fcb247d29
Siti Nuradila <startbeautiful@gmail.com>
2017-05-17 18:10:22 +0800

    6.6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,7 +9,7 @@ def create
   @user = current_user
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
-    
+
     charge = Stripe::Charge.create(
         :amount => (@product.price*100).to_i, # amount in cents, again
         :currency => "euro",
@@ -21,11 +21,11 @@ def create
      Order.create!(product_id: @product.id, user_id: @user.id, total: @product.price)
      redirect_to product_path(@product), notice: 'Purchase was successfully completed.'
    end
-
+end
  rescue Stripe::CardError => e
        # The card has been declined
        body = e.json_body
        err = body[:error]
        flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
      end
-   end
+   
