4d22e875715f68ce7718a253872ec4578e2143e0
Siti Nuradila <startbeautiful@gmail.com>
2017-05-22 00:03:45 +0800

    6.8

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422192437) do
+ActiveRecord::Schema.define(version: 20170517152953) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
@@ -24,9 +24,11 @@ ActiveRecord::Schema.define(version: 20170422192437) do
   end
 
   create_table "orders", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "product_id"
-    t.float   "total"
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.float    "total"
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.index ["product_id"], name: "index_orders_on_product_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
