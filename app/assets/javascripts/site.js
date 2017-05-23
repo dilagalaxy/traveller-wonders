6cebb98d03813dba66a8b07b5edcffa84f0b73bb
Siti Nuradila <startbeautiful@gmail.com>
2017-05-23 23:23:40 +0800

    6.9

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,22 +1,29 @@
-$(document).on('turbolinks:load', function(){
-	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-	$('.rated').raty({ path: '/assets',
-		readOnly: true,
-		score: function() {
-			return $(this).attr('data-score');
-		}
-	});
-	$(".enable-review").click( function() {
-		$(".review-container").show();
-	});
+var refreshRating = function() {
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+    $('.rated').raty({ path: '/assets',
+        readOnly: true,
+        score: function() {
+            return $(this).attr('data-score');
+        }
+    });
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+
+  refreshRating();
 
   // elevate zoom
   $('.img-zoom').elevateZoom({
-  	responsive: true,
-  	zoomWindowWidth:500,
-  	zoomWindowHeight:300,
-  	zoomWindowFadeIn: 500,
-  	zoomWindowFadeOut: 750,
-  	scrollZoom : true
+    responsive: true,
+    zoomWindowWidth:500,
+    zoomWindowHeight:300,
+    zoomWindowFadeIn: 500,
+    zoomWindowFadeOut: 750,
+    scrollZoom: true,
+    zoomLevel:1
   });
-}); 
+
+  $(function() {
+    $('.alert').delay(3000).fadeOut();
+  });
+});
