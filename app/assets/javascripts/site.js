01628197be0bd82ca2d2ec8c4e118badc6e13c89
Siti Nuradila <startbeautiful@gmail.com>
2017-05-23 23:00:38 +0800

    6.9

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,22 @@
 $(document).on('turbolinks:load', function(){
-$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-$('.rated').raty({ path: '/assets',
-readOnly: true,
-score: function() {
-return $(this).attr('data-score');
-}
-});
-   $(".enable-review").click( function() {
-     $(".review-container").show();
-   });
- 
- }); 
+	$('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+	$('.rated').raty({ path: '/assets',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
+	$(".enable-review").click( function() {
+		$(".review-container").show();
+	});
+
+  // elevate zoom
+  $('.img-zoom').elevateZoom({
+  	responsive: true,
+  	zoomWindowWidth:500,
+  	zoomWindowHeight:300,
+  	zoomWindowFadeIn: 500,
+  	zoomWindowFadeOut: 750,
+  	scrollZoom : true
+  });
+}); 
