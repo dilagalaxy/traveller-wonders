6217b5b785d8a0c876315012ea99f1d7a05be912
Siti Nuradila <startbeautiful@gmail.com>
2017-05-26 01:51:33 +0800

    6.11

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,8 +76,8 @@ Rails.application.configure do
     :address => 'smtp.sendgrid.net',
     :port => '587',
     :authentication => :plain,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
+    :user_name => ENV['travellerwonders@heroku.com'],
+    :password => ENV['fb4gol3v5342'],
     :domain => 'heroku.com',
     :enable_starttls_auto => true
   }
