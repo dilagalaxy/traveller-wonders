b7c8ccf35c0de260af742989d20228ce75c1f325
Siti Nuradila <startbeautiful@gmail.com>
2017-06-06 04:34:43 +0700

    6.14

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,23 +1,13 @@
-var refreshRating = function() {
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
+$(document).on('turbolinks:load', function(){
+    $('.rating').raty({ path: "/assets", scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
         readOnly: true,
-        score: function() {
-            return $(this).attr('data-score');
-        }
-    });
-};
-
-$(document).on('turbolinks:load ajaxSuccess', function(){
-
-  refreshRating();
-
-  
-     /* elevate zoom plugin*/
-     $(".img-zoom").elevateZoom({
-      zoomWindowFadeIn: 500,
-      zoomWindowFadeOut: 500,
-      lensFadeIn: 500,
-      lensFadeOut: 500
+      score: function() {
+       return $(this).attr('data-score');
+      }
    });
-  }); 
+
+   $('.img-zoom').elevateZoom({
+    zoomType: "lens", lensShape: "round", lensSize: 200
+   });
+});
